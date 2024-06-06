49bceb47fd2b742bd0b1fc622b2156ac9644ce31
annagonzaga <166526937+annagonzaga@users.noreply.github.com>
2024-06-06 11:56:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+let nome= document.getElementById("nome")
+let resposta= document. getElementById("Anna")
+function aperta()
+{
+  
+  resposta.innerHTML="Olá,"+ nome.value+ "tudo bem?"
+}
+console.log()
+var c=1250
+console.log(c)
+
+let n1=document.getElementById("valor1")
+let n2=document.getElementById("valor2")
+let n3=document.getElementById("valor3")
+let respostadasoma= document.getElementById("resultadodasoma")
+function soma()
+{
+    resposta.innerHTML= parseInt n1.value 
+
+}
+console.clear()
+var a= 1
+while (c>=100)
+  {
+    console.log(a)
+    a=a+1
+
+  }
+{
+  console.clear()
+var a= 0
+while (c>=50)
+
+    console.log(a)
+    a=a+5
+}
+  var b=100;
+  while(b>=0)
+    {
+      console.log(b)
+      b=b-10
+    }
